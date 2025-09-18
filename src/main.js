8480617347ec46703f3f342e09388ac4088bbb30
yassince <charrouferrynda.yassin@gmail.com>
2025-09-18 14:28:21 +0200

    Creating the main html structure, and cleaning and refactor main files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,44 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    Empezando el proyecto
+  <div class="flex items-center border-2 gap-10 border-black justify-center p-6 flex-col">
+    <h1 class="text-3xl font-bold">Password Generator</h1>
+    <section class="min-h-10 min-w-3xs">
+      <div class="min-h-10 min-w-xl border border-black flex justify-around p-6">
+        <span id="pass" class="">Here will be the password</span>
+        <section class="flex gap-4">
+          <button>Copy</button>
+          <button>ReGen</button>
+        </section>
+      </div>
+      <div class="min-h-6 min-w-auto border border-b-2 rounded-b-2xl text-center">There will be the feedback message</div>
+    </section>
+    <section>
+      <from class="flex flex-col gap-6 justify-start border-2 border-red-500 md:min-w-xl">
+        <div class="grid grid-cols-2 justify-center p-6 gap-4">
+          <label for="length">Length</label>
+          <input type="number" value=12 min=8 max=21>
+          <input class="col-span-2" type="range"/>
+        </div>
+
+        <div class="p-6 flex justify-between">
+          <label for="length">Include UpperCase Letter</label>
+          <input type="checkbox"/>
+        </div>
+        <div class="p-6 flex justify-between">
+          <label for="length">Include LowerCase Letter</label>
+          <input type="checkbox"/>
+        </div>
+        <div class="p-6 flex justify-between">
+          <label for="length">Include Symbol</label>
+          <input type="checkbox"/>
+        </div>
+        <div class="p-6 flex justify-between">
+          <label for="length">Include Numbers</label>
+          <input type="checkbox"/>
+        </div>
+
+      </from>
+    </section>
   </div>
 `
