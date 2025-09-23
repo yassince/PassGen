11238d42a1a0f5c74c368438a5b40a143a570e55
yassince <charrouferrynda.yassin@gmail.com>
2025-09-23 22:39:27 +0200

    Finished char class That class will be use to get the char acordign user's  preferences

--- src/utils/Char.js
+++ src/utils/Char.js
@@ -2,21 +2,29 @@
  * Little class for obtaining the necesary char
  */
 export class Char {
-
-    get UpperCase() {
-        //TODO: devolver array UpperCase
+    get upperCase() {
+        return Array.from({ length: 90 - 65 + 1 }, (_, i) => String.fromCharCode(65 + i) )
     }
 
-    get LowerCase() {
-        //TODO: devolver array lowercase
+    get lowerCase() {
+        return Array.from({ length: 122 - 97 + 1 }, (_, i) => String.fromCharCode(97 + i) )
     }
 
-    get Symbol() {
-        //TODO: devolver array symbol
+    get symbol() {
+        let symbol = Array.from({ length: 126 - 33 + 1 }, (_, i) => String.fromCharCode(33 + i))
+
+        //Remove the number
+        symbol.splice(15, 10)
+
+        //Remove upper char
+        symbol.splice(22, 26)
+
+        //Remove lower char
+        symbol.splice(28, 26)
+        return symbol
     }
 
-    get Number() {
-        //TODO: devolver un array only number
+    get number() {
+        return Array.from({ length: 57 - 48 + 1 }, (_, i) => String.fromCharCode(48 + i) )
     }
-
 }
